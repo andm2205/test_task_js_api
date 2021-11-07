7aa367e3f7add812867aa9393fb6acb60f465bb8
Andrey Marchenko <andm22056@gmail.com>
2021-11-07 14:51:24 +0700

    1.0

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# testtask1
+# API для работы с товарами
+
+## Обзор
+
+API для работы с товарами с помощью HTTP-методов реализует определенный функционал. БД содержит 2 сущности: пользователь и товар. В файле `dbstruct.sql` содержится схема БД для СУБД MySQL. По умолчанию сервер работает на порте 3003. В папке `sample` содержатся примеры использования API.
+
+Измените объект `connectionParams` для правильного подключения к вашей БД.
+
+## Запуск
+
+Для запуска откройте терминал и введите:
+
+`node полный_путь_к_файлу_app.js`
+
+## Методы
+
+### POST `/api/create.product`
+
+Позволяет создать товар
+
+Request body: `{name: string, price: number, userId: number, statusCode: number}`
+
+### PUT `/api/update.product`
+
+Позволяет изменить поля товара (название - `name`, цену - `price`, статус - `status` (активный или неактивный)) по параметру `id`. Пользователь указывается для определения, является ли он владельцем. Нельзя изменять товар если он удален или продан.
+
+Request body: `{name: string, price: number, userId: number, statusCode: number, id: number}`
+
+### DELETE `/api/delete.product`
+
+Позволяет удалить товар (изменить статус на 'Удалён') по параметру `id`
+
+Request body: `{id: number}`
+
+### PUT /api/buy.product
+
+Позволяет купить товар по параметру `id`, т. е. изменяет статус товара на 'Продан'. Владелец не может купить свой товар. Купить можно товар только со статусом 'Активен'.
+
+Request body: `{id: number, userId: number}`
+
+### GET `/api/get.product/:id`
+
+Позволяет получить товар по параметру `id`
+
+Response: `Array [{name: string, price: number, userId: number, statusCode: number, id: number}]`
+
+### GET `/api/get.products`
+
+Позволяет получить все товары
+
+Response: `Array [{name: string, price: number, userId: number, statusCode: number, id: number}]`
+
+### GET `/api/get.activeProducts/:userId`
+
+Позволяет получить все активные товары с ID владельца по параметру `userId`
+
+Response: `Array [{name: string, price: number, userId: number, statusCode: number, id: number}]`
--- app.js
+++ app.js
@@ -1,9 +1,9 @@
+const PORT = process.env.PORT ?? 3003
+
 const express = require('express')
-const path = require('path');
-const { stringify } = require('querystring');
 const app = express()
 const bp = require('body-parser');
-const { exception } = require('console');
+const mysql = require('mysql')
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -19,207 +19,236 @@ const connectionParams = {
     host: 'localhost',
     user: 'root',
     database: 'test_task_db',
-    password: '2124'
+    password: '2124',
+    port: 3306
 }
-
-const Statuses = {
-    Active: 1, 
-    Inactive: 2,
-    SoldOut: 3,
-    Deleted: 4
-}
-
-app.get('/', function (req, res) {res.send("hello")})
 
 app.post('/api/create.product', function (req, res) {
-    const product = req.body
-    if(product.name == null
-        || product.price == null
-        || product.userId == null
-        || product.statusCode == null)
-        throw new IllegalArgumentException("param cannot be null");
-    if(product.price < 0)
-        throw new IllegalArgumentException("price cannot be < 0");
-    const mysql = require('mysql2')
     const connection = mysql.createConnection(connectionParams)
-    connection.query(
-        `insert into products (name, price, user_id, status_code) 
-        values (?, ?, ?, ?)`, 
-        [product.name, product.price, product.userId, product.statusCode],
-        function(err, results) {
-            if(err != null)
-                throw err
-            res.status(201);
-        })
-    connection.end()
+    try {
+        const product = req.body
+        if(!(product.name 
+            && product.price 
+            && product.userId 
+            && product.statusCode))
+            throw new Error("param cannot be null")
+        if(product.price < 0)
+            throw new Error("price cannot be < 0");
+        connection.connect()
+        connection.query(
+            `insert into products (name, price, user_id, status_code) 
+            values (?, ?, ?, ?)`, 
+            [product.name, product.price, product.userId, product.statusCode],
+            function(err, results) {
+                if(err)
+                    res.status(500).send(err)
+                else
+                    res.sendStatus(201)
+            })
+    } catch (error) {
+        res.status(500).send({message: error.message})
+    }
 })
 
 app.put('/api/update.product', function (req, res) {
-    const product = req.body
-    if(product.name == null
-        || product.price == null
-        || product.userId == null
-        || product.statusCode == null
-        || product.id == null)
-        throw new IllegalArgumentException("param cannot be null");
-    if(product.price < 0)
-        throw new IllegalArgumentException("price cannot be < 0");
-    if(product.statusCode >= 3 && product.statusCode <= 4)
-        throw new IllegalArgumentException("status code is not valid");
-    const mysql = require('mysql2')
-    const connection = mysql.createConnection(connectionParams)
-    connection.query(
-        `select * from products 
-        where id = ?`,
-        [product.id],
-        function(err, results) {
-            if(err != null)
-                throw err
-            if(results.length === 0)
-                throw new exception('product not found')
-            if(results[0].status_code >= 3 && results[0].status_code <= 4)
-                throw new exception('product is sold or deleted')
-            if(product.userId !== results[0].user_id)
-                throw new exception('access to user denied')
-        })
-    connection.query(
-        `update products
-        set name = ?,
-        price = ?,
-        status_code = ?
-        where id = ?`,
-        [product.name, product.price, product.statusCode, product.id],
-        function(err, results) {
-            if(err != null)
-                throw err
-            res.status(202)
-        })
-    connection.end()
+    var connection = mysql.createConnection(connectionParams)
+    try {
+        const product = req.body
+        if(!(product.name
+            && product.price
+            && product.userId
+            && product.statusCode
+            && product.id))
+            throw new Error("param cannot be null")
+        if(product.price < 0)
+            throw new Error("price cannot be < 0")
+        if(product.statusCode >= 3 && product.statusCode <= 4)
+            throw new Error("status code is not valid")
+        connection.connect()
+        connection.query(
+            `select * from products 
+            where id = ?`,
+            [product.id],
+            function(err, results) {
+                try {
+                    if(err)
+                        res.status(500).send(err)
+                    else if(results.length === 0)
+                        throw new Error('product not found')
+                    else if(results[0].status_code >= 3 && results[0].status_code <= 4)
+                        throw new Error('product is sold or deleted')
+                    else if(product.userId !== results[0].user_id)
+                        throw new Error('access to user denied')
+                    connection.query(
+                        `update products
+                        set name = ?,
+                        price = ?,
+                        status_code = ?
+                        where id = ?`,
+                        [product.name, product.price, product.statusCode, product.id],
+                        function(err, results) {
+                            if(err)
+                                res.status(500).send(err)
+                            else
+                                res.sendStatus(202)
+                        })
+                } catch (error) {
+                    res.status(500).send({message: error.message})
+                }
+            })
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({message: error.message})
+    }
 })
 
 
 app.delete('/api/delete.product', function (req, res) {
-    const productId = req.body.id
-    if(productId == null)
-        throw new IllegalArgumentException("param cannot be null");
-    const mysql = require('mysql2')
     const connection = mysql.createConnection(connectionParams)
-    connection.query(
-        `select * from products
-        where id = ?`,
-        [productId],
-        function(err, results) {
-            if(err != null)
-                throw err
-            if(results.length === 0)
-                throw new exception('product not found')
-            if( results[0].status_code === 4)
-                throw new exception('product is deleted')
-        })
-    connection.query(
-        `delete from products
-        where id = ?`,
-        [productId],
-        function(err, results) {
-            if(err != null)
-                throw err
-            res.status()
-        })
-    connection.end()
+    try {
+        const product = req.body
+        if(product.id == null)
+            throw new Error("param cannot be null");
+        connection.connect()
+        connection.query(
+            `select * from products
+            where id = ?`,
+            [product.id],
+            function(err, results) {
+                try {
+                    if(err)
+                        res.status(500).send(err)
+                    else if(results.length === 0)
+                        throw new Error('product not found')
+                    else if(results[0].status_code === 4)
+                        throw new Error('product is deleted')
+                    else if(product.userId !== results[0].user_id)
+                        throw new Error('access to user denied')
+                    else connection.query(
+                        `delete from products
+                        where id = ?`,
+                        [product.id],
+                        function(err, results) {
+                            if(err)
+                                res.status(500).send(err)
+                            else
+                                res.sendStatus(200)
+                        })
+                } catch (error) {
+                    res.status(500).send({message: error.message})
+                }
+            })
+    } catch (error) {
+        res.status(500).send({message: error.message})
+    }
 })
 
 app.put('/api/buy.product', function (req, res) {
-    const product = req.body
-    if(product == null)
-        throw new IllegalArgumentException("param cannot be null");
-    const mysql = require('mysql2')
     const connection = mysql.createConnection(connectionParams)
-    connection.query(
-        `select * from products
-        where id = ?`,
-        [product.id],
-        function(err, results) {
-            if(err != null)
-                throw err
-            if(results.length === 0)
-                throw new exception('product not found')
-            if(results[0].status_code >= 3 && results[0].status_code <= 4)
-                throw new exception('product is sold or deleted')
-            if(results[0].user_id === product.id)
-                throw new exception('this user is owner of product')
-            if(results[0].status_code === product.statusCode)
-                throw new exception('prosuct is not active')
-        })
-    connection.query(
-        `update products
-        set status_code = 3
-        where id = ?`,
-        [product.id],
-        function(err, results) {
-            if(err != null)
-                throw err
-            res.status(202);
-        })
-    connection.end()
+    try
+    {
+        const product = req.body
+        if(product == null)
+            throw new Error("param cannot be null")
+        connection.connect()
+        connection.query(
+            `select * from products
+            where id = ?`,
+            [product.id],
+             function(err, results) {
+                try {
+                    if(err)
+                        res.status(500).send(err)
+                    else if(results.length === 0)
+                        throw new Error('product not found')
+                    else if(results[0].user_id === product.userId)
+                        throw new Error('this user is owner of product')
+                    else if(results[0].status_code != 1)
+                        throw new Error('product is not active')
+                    else connection.query(
+                        `update products
+                        set status_code = 3
+                        where id = ?`,
+                        [product.id],
+                        function(err, results) {
+                            if(err)
+                                res.status(500).send(err)
+                            else
+                                res.sendStatus(202);
+                        })
+                } catch (error) {
+                    res.status(500).send({message: error.message})
+                }
+            })
+    } catch (error) {
+        res.status(500).send({message: error.message})
+    }
 })
 
 app.get('/api/get.product/:id', function (req, res) {
-    var productId = req.params.id
-    if(productId == null)
-        throw new IllegalArgumentException("param cannot be null");
-    const mysql = require('mysql2')
     const connection = mysql.createConnection(connectionParams)
-    connection.query(
-        `select * from products where id = ?`,
-        [productId],
-        function(err, results) {
-            if(err != null)
-                throw err
-            if(results.length > 0)
-            {
-                console.log(results)
-                res.status(200).json(results)
-            }
-            else
-                res.status(204)
-        })
-    connection.end()
+    try
+    {
+        var productId = req.params.id
+        if(productId == null)
+            throw new Error("param cannot be null")
+        connection.connect()
+        connection.query(
+            `select * from products where id = ?`,
+            [productId],
+            function(err, results) {
+                if(err)
+                    res.status(500).send(err)
+                else if(results.length > 0)
+                    res.status(200).json(results)
+                else
+                    res.sendStatus(204)
+            })
+    } catch (error) {
+        res.status(500).send({message: error.message})
+    }
 })
 
 app.get('/api/get.products', function (req, res) {
-    const mysql = require('mysql2')
     const connection = mysql.createConnection(connectionParams)
-    connection.query(
-        `select * from products`,
-        function(err, results) {
-            if(err != null)
-                throw err
-            res.status(200).json(results)
-        })
-    connection.end()
+    try {
+        connection.connect()
+        connection.query(
+            `select * from products`,
+            function(err, results) {
+                if(err)
+                    res.status(500).send(err)
+                else
+                    res.status(200).json(results)
+            })
+    } catch (error) {
+        res.status(500).send({message: error.message})
+    }
 })
  
 app.get('/api/get.activeProducts/:userId', function (req, res) {
-    const userId = req.params.userId
-    console.log(userId)
-    const mysql = require('mysql2')
     const connection = mysql.createConnection(connectionParams)
-    connection.query(
-        `select * from products
-        where status_code = 1 and user_id = ?`,
-        [userId],
-        function(err, results) {
-            if(err != null)
-                throw err
-            if(results.length > 0)
-                res.status(200).json(results)
-            else
-                res.status(204)
-        })
-    connection.end()
+    try {
+        const userId = req.params.userId
+        connection.connect()
+        connection.query(
+            `select * from products
+            where status_code = 1 and user_id = ?`,
+            [userId],
+            function(err, results) {
+                if(err)
+                    res.status(500).send(err)
+                else if(results.length > 0)
+                    res.status(200).json(results)
+                else
+                    res.status(204).json(results)
+            })
+    } catch (error) {
+        res.status(500).send({message: error.message})
+    }
 })
 
-var server = app.listen(3003, function () {
+var server = app.listen(PORT, function () {
     var address = server.address().address ===  "::" 
     ? "127.0.0.1" 
     : server.address().address;
